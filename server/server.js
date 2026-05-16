46800b863e26f9ca3552f6b4ed90e5997a25eb6f
Brocode69exe <agrimgwari@gmail.com>
2026-05-16 19:25:27 +0530

    Added CLIENT_URL env

--- server/server.js
+++ server/server.js
@@ -9,7 +9,17 @@ dotenv.config()
 
 const app = express()
 
-app.use(cors())
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "http://localhost:5174",
+    "http://localhost:5175",
+    "http://localhost:5176",
+    "https://team-task-manager-x9m9.vercel.app"
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}))
 app.use(express.json())
 app.use("/api/auth", authRoutes)
 app.use("/api/tasks", taskRoutes)
