728f34bf7fa8602fc3b5ad2fe330b8af7c7a00f9
Brocode69exe <agrimgwari@gmail.com>
2026-05-16 18:56:10 +0530

    Added test route

--- server/server.js
+++ server/server.js
@@ -13,6 +13,10 @@ app.use(cors())
 app.use(express.json())
 app.use("/api/auth", authRoutes)
 app.use("/api/tasks", taskRoutes)
+app.get("/api/test", (req, res) => {
+  res.send("API test working")
+})
+
 app.use("/api/projects", projectRoutes)
 
 app.get("/", (req, res) => {
